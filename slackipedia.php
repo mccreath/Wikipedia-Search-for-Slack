8b9f0249de906ec7b6688f2e4acb73d5effdb220
David McCreath <mccreath@gmail.com>
2015-02-16 17:05:29 -0800

    Revert 71b52c0..e6850dc
    This rolls back to commit 71b52c0365db72c27c955aa373586456f20dc824.

--- slackipedia.php
+++ slackipedia.php
@@ -33,11 +33,6 @@ $channel_name = $_POST['channel_name'];
 $user_id = $_POST['user_id'];
 $user_name = $_POST['user_name'];
 
-// override for using on Slack Test. Remove from public version.
-if($token == 'rfszle7MC9JVHZGZ3iib1TZj'){
-	$slack_webhook_url = "https://hooks.slack.com/services/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";  // test
-}
-
 /* 
 	Encode $text for search string 
 */
@@ -50,61 +45,26 @@ $encoded_text = urlencode($text);
 
 $wch_url = "http://".$wiki_lang.".wikipedia.org/w/api.php?action=opensearch&search=".$encoded_text."&format=json&limit=".$search_limit;
 
-$wch = curl_init();
-curl_setopt_array($wch, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $wch_url,
-    CURLOPT_USERAGENT => $user_agent,
-));
+
+$wch = curl_init($wch_url);
+curl_setopt($wch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($wch, CURLOPT_USERAGENT, $user_agent);
 $wch_resp = curl_exec($wch);
+if($wch_resp === FALSE ){
+	$wch_text = "There was a problem reaching Wikipedia. This might be helpful: The cURL error is " . curl_error($ch);
+} else {
+	$wch_text = "";
+}
 curl_close($wch);
 
 
 /*	Handle the returned data from Wikipedia */
 
-$wch_arr = json_decode($wch_resp);
-	
-/*
-	
-	The array that comes back from Wikipedia has 4 nodes.
-	
-	The [0] node in the array is the search string (maybe an array for multi-word strings?)
-	
-	Each subsequent node is an array
-	
-	[1] is page titles (in 'apollo' example, there are 5, 0-4)
-	[2] is page summaries/snippets (again, 0-4)
-	[3] is page URLs (guess what, 0-4)
-	
-	So, for now, we'll do as an attachment:
-	
-	msg text: You searched for [0]. 
-	
-	attachment title	[1][0]
-	attachment text 	[2][0] \n 
-						[3][0] \n
-						*other possibilities*
-						[3][1] \n
-						[3][2] \n
-						[3][3] \n
-						*search again on Wikipedia*
-						<search url>
-	
-	
-	From http://stackoverflow.com/questions/8926805/how-to-use-json-decode-to-extract-an-inner-information
+if($wch_resp !== FALSE){
 
-	Pass TRUE to json_decode as second parameter
-
-	$output = json_decode($input,TRUE);
-	Then traverse the arrays. It should be something like $output['g'][0]['g9']['text3']['g91']
-*/
-
-
-
+	$wch_arr = json_decode($wch_resp);
 	$other_options = $wch_arr[3];
-
 	$first_item = array_shift($other_options);
-
 	$other_options_count = count($other_options);
 	
 	$wch_text = "<@".$user_id."|".$user_name."> searched for *".$text."*.\n";
@@ -112,8 +72,8 @@ $wch_arr = json_decode($wch_resp);
 	$disamb_check = "may refer to:";
 
 	$wch_att_title	= 	$wch_arr[1][0];
-	$wch_att_desc = $wch_arr[2][0];
-	$wch_att_link = $wch_arr[3][0];
+	$wch_att_desc		=		$wch_arr[2][0];
+	$wch_att_link		=		$wch_arr[3][0];
 
 	if(count($wch_arr[1]) == 0){
 		$wch_att_text = "Sorry! I couldn't find anything like that.";
@@ -135,6 +95,8 @@ $wch_arr = json_decode($wch_resp);
 			$wch_att_other .= $value."\n";
 		}
 	}
+
+}
 	
 // Send it back through the webhook
 $data = array(
@@ -175,6 +137,43 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 );                                                                                                                   
 $result = curl_exec($ch);
 curl_close($ch);
+
+
+
+/*
+	
+	The array that comes back from Wikipedia has 4 nodes.
+	
+	The [0] node in the array is the search string (maybe an array for multi-word strings?)
+	
+	Each subsequent node is an array
+	
+	[1] is page titles (in 'apollo' example, there are 5, 0-4)
+	[2] is page summaries/snippets (again, 0-4)
+	[3] is page URLs (guess what, 0-4)
+	
+	So, for now, we'll do as an attachment:
+	
+	msg text: You searched for [0]. 
+	
+	attachment title	[1][0]
+	attachment text 	[2][0] \n 
+						[3][0] \n
+						*other possibilities*
+						[3][1] \n
+						[3][2] \n
+						[3][3] \n
+						*search again on Wikipedia*
+						<search url>
+	
+	
+	From http://stackoverflow.com/questions/8926805/how-to-use-json-decode-to-extract-an-inner-information
+
+	Pass TRUE to json_decode as second parameter
+
+	$output = json_decode($input,TRUE);
+	Then traverse the arrays. It should be something like $output['g'][0]['g9']['text3']['g91']
+*/
 
 
 
