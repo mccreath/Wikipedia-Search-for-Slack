ac41ffffc7ab4aa7c5b0968eea23fb1395bc54ea
David McCreath <mccreath@gmail.com>
2015-03-16 22:59:02 -0700

    updating documentation

--- README.md
+++ README.md
@@ -6,3 +6,25 @@ Requirements
 * custom slash command on Slack
 * internal webhook on Slack
 * a web server running PHP 5
+
+Comments in the script will walk you through the setup if you're familiar with those three things. There's also a tutorial in this README.
+
+
+## Tutorial
+
+This very simple demo will walk you through the process of setting up both a custom slash command (https://api.slack.com/slash-commands) and an incoming webhook (https://api.slack.com/incoming-webhooks).
+
+Wikipedia is good to use for this because you don't need an API key to access their search API. All you need to do is identify your script with a user agent string (which we'll cover in a bit). In fact, because this is the default for all MediaWiki installations, you could repurpose this script to search _any_ site built on MediaWiki.
+
+## What you'll need:
+
+* A hosting account running PHP 5 where you can put the script we're going to write
+* A Slack account (a free one is fine)
+
+
+## The Script
+
+
+## Set up your slash command
+
+Go to your integrations page at Slack (http://my.slack.com/services/new) and scroll down to the bottom section, "DIY Integrations & Customizations". Click on the "Add" button for "Slash Commands"
--- slackipedia.php
+++ slackipedia.php
@@ -1,29 +1,26 @@
 <?php
 
-/*
-	Config.
-*/
+/*	Config.  */
 
+//  Slack webhook URL - from the webhook config page
+$slack_webhook_url = "https://hooks.slack.com/services/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";
 
-// Slack webhook URL, from the webhook config page
-$slack_webhook_url = "https://hooks.slack.com/services/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";  //dahveedtest
+//  Icon URL - Where ever you put the from the zip archive
+$icon_url = "http://dmccreath.org/slackipedia/wikipedia-logo-cc-by-sa_0.png";
 
-// prepended to search URL to determine which language you're searching in
+//  prepended to search URL to determine which language you're searching in
 $wiki_lang = "en";
-// WikiMedia API defaults to 10. This defaults to 4, so you get 1 result and 3 other options. Do what thou wilt.
+//  WikiMedia API defaults to 10. This defaults to 4, so you get 1 result and 3 other options. Do what thou wilt.
 $search_limit = "4"; 
 
-/*
-	The WikiMedia API requests that the client is identified by a User-Agent string
-	http://www.mediawiki.org/wiki/API:Etiquette#User-Agent_header
-*/
-
+//	The WikiMedia API requests that the client is identified by a User-Agent string
+//	http://www.mediawiki.org/wiki/API:Etiquette#User-Agent_header
 $user_agent = "Slackipedia/1.0 (https://github.com/mccreath/slackipedia; mccreath@gmail.org)";
 
-/* 
-	Grab values from the slash command, create vars for post back to webhook
-*/
 
+/*  Now for some action! */
+
+//  Grab the POST values from the slash command, create vars for post back to webhook
 $command = $_POST['command'];
 $text = $_POST['text'];
 $token = $_POST['token'];
@@ -33,66 +30,59 @@ $channel_name = $_POST['channel_name'];
 $user_id = $_POST['user_id'];
 $user_name = $_POST['user_name'];
 
-/* 
-	Encode $text for search string 
-*/
-
+//  Encode the $text for the Wikipedia search string 
 $encoded_text = urlencode($text);
 
-/*
-	Call Wikipedia API with cURL (must be a GET)
-*/
+//  Create URL for Wikipedia API, which requires using GET
+$wiki_url = "http://".$wiki_lang.".wikipedia.org/w/api.php?action=opensearch&search=".$encoded_text."&format=json&limit=".$search_limit;
 
-$wch_url = "http://".$wiki_lang.".wikipedia.org/w/api.php?action=opensearch&search=".$encoded_text."&format=json&limit=".$search_limit;
-
-
-$wch = curl_init($wch_url);
-curl_setopt($wch, CURLOPT_RETURNTRANSFER, true); 
-curl_setopt($wch, CURLOPT_USERAGENT, $user_agent);
-$wch_resp = curl_exec($wch);
-if($wch_resp === FALSE ){
-	$wch_text = "There was a problem reaching Wikipedia. This might be helpful: The cURL error is " . curl_error($ch);
+//  Call the URL
+$wiki = curl_init($wiki_url);
+curl_setopt($wiki, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($wiki, CURLOPT_USERAGENT, $user_agent);
+$wiki_resp = curl_exec($wiki);
+if($wiki_resp === FALSE ){
+	$wiki_text = "There was a problem reaching Wikipedia. This might be helpful: The cURL error is " . curl_error($wiki);
 } else {
-	$wch_text = "";
+	$wiki_text = "";
 }
-curl_close($wch);
+curl_close($wiki);
 
 
-/*	Handle the returned data from Wikipedia */
+//  Handle the returned data from Wikipedia
+if($wiki_resp !== FALSE){
 
-if($wch_resp !== FALSE){
-
-	$wch_arr = json_decode($wch_resp);
-	$other_options = $wch_arr[3];
+	$wiki_arr = json_decode($wiki_resp);
+	$other_options = $wiki_arr[3];
 	$first_item = array_shift($other_options);
 	$other_options_count = count($other_options);
 	
-	$wch_text = "<@".$user_id."|".$user_name."> searched for *".$text."*.\n";
+	$wiki_text = "<@".$user_id."|".$user_name."> searched for *".$text."*.\n";
 
 	$disamb_check = "may refer to:";
 
-	$wch_att_title	= 	$wch_arr[1][0];
-	$wch_att_desc		=		$wch_arr[2][0];
-	$wch_att_link		=		$wch_arr[3][0];
+	$wiki_att_title	= 	$wiki_arr[1][0];
+	$wiki_att_desc		=		$wiki_arr[2][0];
+	$wiki_att_link		=		$wiki_arr[3][0];
 
-	if(count($wch_arr[1]) == 0){
-		$wch_att_text = "Sorry! I couldn't find anything like that.";
+	if(count($wiki_arr[1]) == 0){
+		$wiki_att_text = "Sorry! I couldn't find anything like that.";
 	} else {
-		$wch_att_text = "";
-		$wch_att_other = "";
-		if (strpos($wch_arr[2][0],$disamb_check) !== false) { // see if it's a disambiguation page
-			$wch_text	.= "There are several possible results for ";
-			$wch_text	.= "*<".$wch_att_link."|".$text.">*.\n";
-			$wch_text	.= $wch_att_link;
-			$wch_att_other_title = "Here are some of the possibilities:";
+		$wiki_att_text = "";
+		$wiki_att_other = "";
+		if (strpos($wiki_arr[2][0],$disamb_check) !== false) { // see if it's a disambiguation page
+			$wiki_text	.= "There are several possible results for ";
+			$wiki_text	.= "*<".$wiki_att_link."|".$text.">*.\n";
+			$wiki_text	.= $wiki_att_link;
+			$wiki_att_other_title = "Here are some of the possibilities:";
 		} else {
-			$wch_text	.= 	"*<".$wch_att_link."|".$wch_att_title.">*\n";
-			$wch_text	.= 	$wch_att_desc."\n";
-			$wch_text	.= 	$wch_att_link;
-			$wch_att_other_title 	= 	"Here are a few other options:";
+			$wiki_text	.= 	"*<".$wiki_att_link."|".$wiki_att_title.">*\n";
+			$wiki_text	.= 	$wiki_att_desc."\n";
+			$wiki_text	.= 	$wiki_att_link;
+			$wiki_att_other_title 	= 	"Here are a few other options:";
 		}
 		foreach ($other_options as $value) {
-			$wch_att_other .= $value."\n";
+			$wiki_att_other .= $value."\n";
 		}
 	}
 
@@ -102,23 +92,23 @@ if($wch_resp !== FALSE){
 $data = array(
 	"username" => "Slackipedia",
 	"channel" => $channel_id,
-	"text" => $wch_text,
+	"text" => $wiki_text,
  	"mrkdwn" => true,
- 	"icon_url" => "http://dmccreath.org/slackipedia/wikipedia-logo-cc-by-sa_0.png",
+ 	"icon_url" => $icon_url,
  	"attachments" => array(
  		 array(
 			"color" => "#b0c4de",
- 		//	"title" => $wch_att_title,
- 			"fallback" => $wch_att_text,
- 			"text" => $wch_att_text,
+ 		//	"title" => $wiki_att_title,
+ 			"fallback" => $wiki_att_text,
+ 			"text" => $wiki_att_text,
  			"mrkdwn_in" => array(
  				"fallback",
  				"text"
  			),
  			"fields" => array(
  				array(
- 					"title" => $wch_att_other_title,
- 					"value" => $wch_att_other
+ 					"title" => $wiki_att_other_title,
+ 					"value" => $wiki_att_other
  				)
  			)
  		)
